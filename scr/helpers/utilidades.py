a1b9dbef5601360eda4be9f1ff2e1c0cf81c0443
SharonOG23 <sharonobandogomez178@gmail.com>
2025-12-09 13:16:16 -0600

    RAMA -> HELPERS
    feat: Se crea la clase de helpers para tener funciones auxiliares

--- scr/helpers/utilidades.py
+++ scr/helpers/utilidades.py
@@ -1,4 +1,25 @@
-#Contiene funciones auxiliares reutilizables para validaciones, formateo, etc.
+#Utilidades: Contiene funciones auxiliares reutilizables para validaciones, formateo, etc.
 
+# librerias
+import pandas as pd
+
+# Clase utilidades
 class Utilidades:
-    pass
+
+        @staticmethod
+        def cargar_csv(ruta):  # carga archivo
+            try:
+                df = pd.read_csv(ruta)  # lee el csv
+                print(f"CSV cargado: {ruta}")
+                return df
+            except Exception as e:
+                print(f"Error cargando CSV: {e}")  # imprime el error si hay
+                return None
+
+        @staticmethod
+        def eliminar_duplicados(df):  # elimina los valores duplicados
+            return df.drop_duplicates()  # devuelve sin duplicados
+
+        @staticmethod
+        def rellenar_nulos(df, valor=0):  # rellena los valores nulos
+            return df.fillna(valor)
