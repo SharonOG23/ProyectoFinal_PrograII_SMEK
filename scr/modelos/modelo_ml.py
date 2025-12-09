52725ac43743176b1f6f1a67c4d190118fc37c10
SharonOG23 <sharonobandogomez178@gmail.com>
2025-12-09 13:13:18 -0600

    MODELO ML
    feat: Se realizan unos cambios al modelo ml

--- scr/modelos/modelo_ml.py
+++ scr/modelos/modelo_ml.py
@@ -1,26 +1,63 @@
-#Entrena y evalúa modelos supervisados (regresión o clasificación).
+#Modelo: Entrena y evalúa modelos supervisados (regresión o clasificación).
 
-# carga librerias
-from sklearn.model_selection import train_test_split # divide los datos
-from sklearn.metrics import mean_squared_error # calcula el error
-from sklearn.linear_model import LinearRegression # modelo de regresion lineal
+# Librerías
+from sklearn.model_selection import train_test_split   # divide datos
+from sklearn.metrics import mean_squared_error         # calcula error
+from sklearn.linear_model import LinearRegression      # regresión lineal
+from sklearn.neighbors import KNeighborsRegressor      # KNN
+from sklearn.ensemble import RandomForestRegressor     # Random Forest
+import pandas as pd
 
 # Clase ModeloML
-
 class ModeloML:
-    def __init__(self, df, columna_objetivo):
-        self.df = df.copy()  #  guarda copia del original
-        self.columna_objetivo = columna_objetivo
-        self.modelo = LinearRegression() # modelo de regresion lineal
+
+    @staticmethod
+    def cargar_csv(ruta): # carga archivo
+        try:
+            df = pd.read_csv(ruta) # lee el archivo
+            print(f"CSV cargado: {ruta}")
+            return df
+        except Exception as e:
+            print(f"Error cargando CSV: {e}") # imprime el error
+            return None
+
+    def _init_(self, df, columna_objetivo): # constructor
+        self.df = df.copy()             # copia del dataframe
+        self.columna_objetivo = columna_objetivo  # columna a trabajar
+
+        # Modelos
+        self.modelos = {
+            "Regresion_Lineal": LinearRegression(),
+            "KNN": KNeighborsRegressor(n_neighbors=3),
+            "Random_Forest": RandomForestRegressor(n_estimators=100, random_state=42)
+        }
+
+# Preparacion de datos
 
     def preparar_datos(self):
-        X = self.df.drop(columns=[self.columna_objetivo])
-        y = self.df[self.columna_objetivo]
-        return train_test_split(X, y, test_size=0.2, random_state=42) # divide valores
+        """Divide los datos en entrenamiento y prueba."""
+        X = self.df.drop(columns=[self.columna_objetivo])  # variables de entrada
+        y = self.df[self.columna_objetivo]                 # objetivo
 
-    def entrenar(self):   # entrenamiento
+        return train_test_split(X, y, test_size=0.2, random_state=42)
+
+    def entrenar_todos(self):
+        X_train, X_test, y_train, y_test = self.preparar_datos() # entrena los modelos
+        resultados = {}
+
+        for nombre, modelo in self.modelos.items(): #bucle que recorre los modelos para otener los datos a necesitar
+            modelo.fit(X_train, y_train)              # entrenamiento
+            y_pred = modelo.predict(X_test)           # predicción
+            mse = mean_squared_error(y_test, y_pred)  # error
+            resultados[nombre] = mse                  # guarda
+
+        return resultados
+
+    def entrenar(self):
+    # Modelo de regresion lineal
         X_train, X_test, y_train, y_test = self.preparar_datos()
-        self.modelo.fit(X_train, y_train)
-        y_pred = self.modelo.predict(X_test)    # hace predicciones
+        modelo = LinearRegression()
+        modelo.fit(X_train, y_train)
+        y_pred = modelo.predict(X_test)
         mse = mean_squared_error(y_test, y_pred)
-        return self.modelo, mse  # devuelve modelo entrenado
+        return modelo, mse
