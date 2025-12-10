82418d0e3cb77f6520544258d30374972a2fb43a
SharonOG23 <sharonobandogomez178@gmail.com>
2025-12-09 23:42:07 -0600

    Actualizacion Modelo y Helpers
    feat: Se agrega a las clases la documentacion en cada linea

--- scr/helpers/utilidades.py
+++ scr/helpers/utilidades.py
@@ -1,25 +1,25 @@
-#Utilidades: Contiene funciones auxiliares reutilizables para validaciones, formateo, etc.
+#Contiene funciones auxiliares reutilizables para validaciones, formateo, etc.
 
-# librerias
+# importa librerias
 import pandas as pd
 
 # Clase utilidades
 class Utilidades:
 
         @staticmethod
-        def cargar_csv(ruta):  # carga archivo
-            try:
-                df = pd.read_csv(ruta)  # lee el csv
-                print(f"CSV cargado: {ruta}")
-                return df
-            except Exception as e:
-                print(f"Error cargando CSV: {e}")  # imprime el error si hay
-                return None
+        def cargar_csv(ruta):                   # carga archivo
+            try:                                 # inicia un bloque para manejar errores
+                df = pd.read_csv(ruta)           # lee el csv
+                print(f"CSV cargado: {ruta}")     # imprime un mensaje de carga exitosa
+                return df                          # devuelve el df cargado
+            except Exception as e:                # si hay un error, lo caotura y lo guarda en "e"
+                print(f"Error cargando CSV: {e}")     # imprime el error si hay
+                return None            # si hay un error muestra none para indicar que no se realizo bien el proceso
 
         @staticmethod
-        def eliminar_duplicados(df):  # elimina los valores duplicados
-            return df.drop_duplicates()  # devuelve sin duplicados
+        def eliminar_duplicados(df):              # elimina los valores duplicados
+            return df.drop_duplicates()            # devuelve el df sin duplicados
 
         @staticmethod
-        def rellenar_nulos(df, valor=0):  # rellena los valores nulos
-            return df.fillna(valor)
+        def rellenar_nulos(df, valor=0):              # metodo para rellenar los valores nulos
+            return df.fillna(valor)               # devuelve el df con los valores nulos rellenados
--- scr/modelos/modelo_ml.py
+++ scr/modelos/modelo_ml.py
@@ -1,11 +1,11 @@
-#Modelo: Entrena y evalúa modelos supervisados (regresión o clasificación).
+# Entrena y evalúa modelos supervisados (regresión o clasificación).
 
 # Librerías
-from sklearn.model_selection import train_test_split   # divide datos
-from sklearn.metrics import mean_squared_error         # calcula error
-from sklearn.linear_model import LinearRegression      # regresión lineal
-from sklearn.neighbors import KNeighborsRegressor      # KNN
-from sklearn.ensemble import RandomForestRegressor     # Random Forest
+from sklearn.model_selection import train_test_split        # divide datos
+from sklearn.metrics import mean_squared_error              # calcula errores
+from sklearn.linear_model import LinearRegression           # modelo regresión lineal
+from sklearn.neighbors import KNeighborsRegressor           # modelo KNN
+from sklearn.ensemble import RandomForestRegressor          # modelo Random Forest
 import pandas as pd
 
 # Clase ModeloML
@@ -15,49 +15,48 @@ class ModeloML:
     def cargar_csv(ruta): # carga archivo
         try:
             df = pd.read_csv(ruta) # lee el archivo
-            print(f"CSV cargado: {ruta}")
-            return df
-        except Exception as e:
+            print(f"CSV cargado: {ruta}")  # imprime csv cargado
+            return df   # retorna la variable df
+        except Exception as e: # captura error y lo guarda em "e"
             print(f"Error cargando CSV: {e}") # imprime el error
-            return None
+            return None         # si hay error, imprime un mensaje de que no se realizo satisfactoriamente
 
-    def _init_(self, df, columna_objetivo): # constructor
-        self.df = df.copy()             # copia del dataframe
-        self.columna_objetivo = columna_objetivo  # columna a trabajar
+    def _init_(self, df, columna_objetivo):           # constructor
+        self.df = df.copy()                             # copia del dataframe
+        self.columna_objetivo = columna_objetivo         # columna a trabajar
 
         # Modelos
         self.modelos = {
-            "Regresion_Lineal": LinearRegression(),
-            "KNN": KNeighborsRegressor(n_neighbors=3),
-            "Random_Forest": RandomForestRegressor(n_estimators=100, random_state=42)
+            "Regresion_Lineal": LinearRegression(),        # crea modelo de regresion lineal
+            "KNN": KNeighborsRegressor(n_neighbors=3),        # crea modelo KNN
+            "Random_Forest": RandomForestRegressor(n_estimators=100, random_state=42)  # crea modelo Random forest
         }
 
 # Preparacion de datos
 
-    def preparar_datos(self):
-        """Divide los datos en entrenamiento y prueba."""
-        X = self.df.drop(columns=[self.columna_objetivo])  # variables de entrada
-        y = self.df[self.columna_objetivo]                 # objetivo
+    def preparar_datos(self):  #define metodo
+        X = self.df.drop(columns=[self.columna_objetivo])         # variables de entrada quita la clumana objetivo
+        y = self.df[self.columna_objetivo]                        # columna objetivo, que se va a usar o que queremos predecir
 
-        return train_test_split(X, y, test_size=0.2, random_state=42)
+        return train_test_split(X, y, test_size=0.2, random_state=42)        # divide datos em entrenamiento y prueba
 
-    def entrenar_todos(self):
-        X_train, X_test, y_train, y_test = self.preparar_datos() # entrena los modelos
-        resultados = {}
+    def entrenar_todos(self):       # metodo que entrena todos los modelos
+        X_train, X_test, y_train, y_test = self.preparar_datos()      # entrena los modelos
+        resultados = {}           # se guarda aca los resultados de los metodos
 
         for nombre, modelo in self.modelos.items(): #bucle que recorre los modelos para otener los datos a necesitar
-            modelo.fit(X_train, y_train)              # entrenamiento
-            y_pred = modelo.predict(X_test)           # predicción
-            mse = mean_squared_error(y_test, y_pred)  # error
-            resultados[nombre] = mse                  # guarda
+            modelo.fit(X_train, y_train)              # entrenamiento con los datos de entrenamiento
+            y_pred = modelo.predict(X_test)           # predicción usando los datos de prueba
+            mse = mean_squared_error(y_test, y_pred)  # error, hace comparacion de predicciones con las variables
+            resultados[nombre] = mse                  # guarda el resultado
 
-        return resultados
+        return resultados  # devuelve los resultados de todos los modelos evaluados
 
     def entrenar(self):
     # Modelo de regresion lineal
-        X_train, X_test, y_train, y_test = self.preparar_datos()
-        modelo = LinearRegression()
-        modelo.fit(X_train, y_train)
-        y_pred = modelo.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-        return modelo, mse
+        X_train, X_test, y_train, y_test = self.preparar_datos() # llama al metodo prepara_datos
+        modelo = LinearRegression()  # crea la instancia del modelo de regresion lineal
+        modelo.fit(X_train, y_train) # relacion con las variables
+        y_pred = modelo.predict(X_test) # predice valores de y para x
+        mse = mean_squared_error(y_test, y_pred) # mide los errores del modelo
+        return modelo, mse   # devuelve el modelo entrenado listo y errores de evaluacion
