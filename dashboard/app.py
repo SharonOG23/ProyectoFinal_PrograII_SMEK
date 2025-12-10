ede5df6286f7fb529a46cfe9996b46acf8adf5fd
Kris11234567 <menakristel85@gmail.com>
2025-12-10 11:39:30 -0600

    Dashboard
    feat:documentacion del dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,19 +8,21 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 
-from scr.visualizacion.visualizador import visualizador
+from scr.visualizacion.visualizador import visualizador #Aqui importamos la clase y el metodo para poder generar el dashboard
 from streamlit_folium import st_folium
 
 # ===== Rutas de tus CSV =====
+#Metemos las rutas de los archivos en variables
 RUTA_COORDENADAS = os.path.join(BASE_DIR, "data", "processed", "Coordenadas_Paises.csv")
 RUTA_TENDENCIA   = os.path.join(BASE_DIR, "data", "processed", "turismo_anios_clean.csv")
 RUTA_HEATMAP     = os.path.join(BASE_DIR, "data", "processed", "turismo_anios_clean.csv")
 RUTA_BARRAS      = os.path.join(BASE_DIR, "data", "processed", "zonas_aereas_clean.csv")
 
 # ===== Dashboard =====
+#Parte estetica del dashboard, sus titulos correspondientes
 st.title("Dashboard de Turismo en Costa Rica")
 st.sidebar.title("Menú de visualizaciones")
-
+#Generamos una caja para que se vea mas ordenado, esta nos va a dejar seleccionar ver solo el grafico que necesitamos
 opcion = st.sidebar.selectbox(
     "Selecciona un gráfico",
     [
@@ -30,18 +32,18 @@ opcion = st.sidebar.selectbox(
         "Barras Horizontales por Zona (Automático)"
     ]
 )
-
+#Aqui generamos tipo un menu para poder visualizar los graficos
 # ================================
 # MAPA DE PAÍSES (INTERACTIVO)
 # ================================
 if opcion == "Mapa de Países":
-    st.subheader(" Ubicación geográfica de países visitantes")
+    st.subheader(" Ubicación geográfica de países visitantes") #Se usa para mostrar el texto como un encabezado secundario
 
     viz = visualizador(RUTA_COORDENADAS)
-    mapa = viz.mapa_paises()  # Folium map
+    mapa = viz.mapa_paises()
 
     # Mostrar en Streamlit
-    st_folium(mapa, width=900, height=600)
+    st_folium(mapa, width=900, height=600) #usamos st_folium para mostrar el mapa dentro de streamlit
 
 # ================================
 # TENDENCIA TOTAL
